fdf5afdd2753aaa40e2ea8a86290f56b46edce9f
Ian <holmesian17@gmail.com>
2021-12-11 22:49:53 -0600

    Update basic_math_functions.cpp

--- basic_math_functions.cpp
+++ basic_math_functions.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 // This is a comment
 /*
-  and this is a multi-line comment.
-  See?
+  * and this is a multi-line comment.
+  * See?
 */
 
 int main()
@@ -11,13 +11,13 @@ int main()
   int v1 = 0, v2 = 0;
   std::cin >> v1 >> v2;
   std::cout << "The division of " << v1 << " by " << v2 << " divided is "
-            << v1 / v2 << " with a remainder of " << v1 % v2 << std::endl;
+            << v1 / v2 << " with a remainder of " << v1 % v2 << "."<< std::endl;
   std::cout << "The product of " << v1 << " and "<< v2 << " is "
-            << v1 * v2 << std::endl;
+            << v1 * v2 << "." << std::endl;
   std::cout << "The remainder of " << v1 << " minus "<< v2 << " is "
-            << v1 - v2 << std::endl;
+            << v1 - v2 << "." << std::endl;
   std::cout << "The sum of " << v1 << " and "<< v2 << " is "
-            << v1 + v2 << std::endl;
+            << v1 + v2 << "." << std::endl;
 
   return 0;
 }
